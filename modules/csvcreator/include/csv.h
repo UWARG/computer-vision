662aa57b6b43250e4c3364b82dd19783ef7f4da8
Christopher Hajduk <chrishajduk84@hotmail.com>
2015-09-29 19:43:40 -0400

    Included GPL licensing statements

--- modules/csvcreator/include/csv.h
+++ modules/csvcreator/include/csv.h
@@ -1,3 +1,20 @@
+/*This file is part of WARG's computer-vision.
+
+    computer-vision is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version. You cannot however, claim it as your own. 
+	Any code used MUST be referenced to WARG (Waterloo Aerial Robotics Group). 
+	Additionally, this code cannot be used in any competition against WARG.
+
+    computer-vision is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with computer-vision.  If not, see <http://www.gnu.org/licenses/>.
+	*/
 #ifndef CSV_H_INCLUDED
 #define CSV_H_INCLUDED
 
